0eea9efd74af4b019b7712430bc31a0ebed21717
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-08-10 17:03:58 +0200

    Project Type Schema and queries, types relationships

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -19,9 +19,33 @@ const ClientType = new GraphQLObjectType({
   }),
 });
 
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    clientId: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
@@ -29,10 +53,17 @@ const RootQuery = new GraphQLObjectType({
         return clients.find((client) => client.id === args.id);
       },
     },
-    clients: {
-      type: new GraphQLList(ClientType),
+    projects: {
+      type: new GraphQLList(ProjectType),
       resolve(parent, args) {
-        return clients;
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find((project) => project.id === args.id);
       },
     },
   },
