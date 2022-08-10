f84ad0cb3e415f09403f30854291d1e2e5aaad8e
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-08-10 16:52:31 +0200

    Client Type Schema and queries

---
+++ server/schema/schema.js
@@ -0,0 +1,41 @@
+const { projects, clients } = require('../sampleData.js');
+
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+} = require('graphql');
+
+// Client Type
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return clients.find((client) => client.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
