91013208583d908ba1a1f95c063f47585e58843a
Dawid Marcinkowski <marcinkowski.dawidd@gmail.com>
2022-08-11 12:30:19 +0200

    Update project mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -149,6 +149,39 @@ const Mutation = new GraphQLObjectType({
         return project;
       },
     },
+    // Update a project
+    updateProject: {
+      type: ProjectType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: {
+          type: new GraphQLEnumType({
+            name: 'ProjectStatusUpdate',
+            values: {
+              new: { value: 'Not Started' },
+              progress: { value: 'In Progress' },
+              completed: { value: 'Completed' },
+            },
+          }),
+        },
+      },
+      resolve(parent, args) {
+        const project = Project.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true }
+        );
+        return project;
+      },
+    },
   },
 });
 
